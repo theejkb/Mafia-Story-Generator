5bc964b9b498fa08adf7929b38cef4d2fb3a9f02
NaaQ <stevencopy@hotmail.fr>
2023-07-10 18:10:30 +0200

    feat: add twitter share

--- components/Screenshotable/Screenshotable.js
+++ components/Screenshotable/Screenshotable.js
@@ -1,46 +1,61 @@
 import React, { createRef } from "react";
-import PropTypes from "prop-types";
 import { useScreenshot, createFileName } from "use-react-screenshot";
 import * as htmlToImage from "html-to-image";
 
 const Screenshot = ({ children }) => {
-        const [image] = useScreenshot({
-            type: "image/png",
-            quality: 1.0,
-        });
-        const ref = createRef(null);
+  const [image] = useScreenshot({
+    type: "image/png",
+    quality: 1.0,
+  });
+  const ref = createRef(null);
 
-        const download = (image, { name = "mobsters", extension = "png" } = {}) => {
-            const a = document.createElement("a");
-            a.href = image;
-            a.download = createFileName(extension, name);
-            a.click();
-        };
+  const download = (image, { name = "mobsters", extension = "png" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    // Obtenir l'URL de l'image générée à partir du canvas
+    a.download = createFileName(extension, name);
+    a.click();
+  };
 
-        const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
+  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
-        const takeScreenShot = async(node) => {
-            const dataURI = await htmlToImage.toJpeg(node);
-            return dataURI;
-        };
+  const takeScreenShot = async (node) => {
+    const dataURI = await htmlToImage.toJpeg(node);
+    return dataURI;
+  };
 
-        return (
-            <div>
-              <div ref={ref}>{children}</div>
-              <div className="text-center mb-4 mt-5">
-                { <button
-                  onClick={downloadScreenshot}
-                  className="px-2 py-1 rounded bg-red-500 text-white"
-                >
-                  Download image
-                </button> }
-              </div>
-            </div>
-          );
-        };
+  return (
+    <div>
+      <div ref={ref}>{children}</div>
+      <div className="text-center mb-4 mt-5">
+        {<button
+          onClick={downloadScreenshot}
+          className="px-2 py-1 rounded bg-red-500 text-white"
+        >
+          Download image
+        </button>}
+      </div>
+      <div className="text-center mb-4 mt-5">
+        {<button className="text-white tweet py1 px-2 rounded"
+        >
+          <a
+            href="https://twitter.com/intent/tweet?text=I have just created my own @Elrond_Mafia Story. Check it out now : https://mafia-story-generator.vercel.app/"
+            target='_blank'
+            rel="noreferrer"
+            data-size="large">
+            Share on Tweeter</a>
+        </button>}
+      </div>
+      <div className="">
 
-        Screenshot.propTypes = {
-            //
-        };
+      </div>
 
-        export default Screenshot;
+    </div>
+  );
+};
+
+Screenshot.propTypes = {
+  //
+};
+
+export default Screenshot;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import _Layout from "../components/_Layout";
 import "../styles/globals.css";
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <_Layout>
